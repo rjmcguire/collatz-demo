d1f6d890bb55efc8727e2c94f9908a3a0521ca6d
Rory McGuire <rjmcguire@gmail.com>
2017-08-30 15:21:41 +0200

    add multi-threading

--- main.c
+++ main.c
@@ -3,12 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <pthread.h>
 
 #include "collatz.h"
 #include "cache.h"
 
 struct CacheEntry main1(int start, int end);
+void* thread_main(void *arg);
 
+struct WorkEntry {
+	int upper;
+	int lower;
+	struct CacheEntry result;
+};
 
 int main(int argc, char **argv) {
 	int n;
@@ -24,26 +31,42 @@ int main(int argc, char **argv) {
 	work_set_size = ceil(n / (double)MAX_THREADS);
 	// split the numbers into sets then
 	// run the numbers through a set of workers
-	int lower, upper;
-	struct CacheEntry results[MAX_THREADS];
+	struct WorkEntry results[MAX_THREADS];
+	pthread_t workers[MAX_THREADS];
 	for (int i=0; i<MAX_THREADS; i++) {
-		lower = (work_set_size * i)+1;
-		upper   = (work_set_size * (i+1));
-		printf("%d vs %d\n", upper, n);
-		if (upper > n) {
-			upper = n;
+		results[i].lower = (work_set_size * i)+1;
+		results[i].upper   = (work_set_size * (i+1));
+		printf("%d vs %d\n", results[i].upper, n);
+		if (results[i].upper > n) {
+			results[i].upper = n;
 		}
-		printf("work upper:%d lower:%d\n", upper, lower);
-		results[i] = main1(upper, lower);
+		printf("work upper:%d lower:%d\n", results[i].upper, results[i].lower);
+
+		// results[i] = main1(upper, lower);
+		if(pthread_create(&workers[i], NULL, thread_main, &results[i])) {
+			fprintf(stderr, "Error creating thread\n");
+			return 1;
+		}
+	}
+	for (int i=0; i<MAX_THREADS; i++) {
+		pthread_join(workers[i], NULL);
 	}
 
 	struct CacheEntry result = { .number=0, .length=0};
 	for (int i=0; i<MAX_THREADS; i++) {
-		if (result.length < results[i].length) {
-			result = results[i];
+		if (result.length < results[i].result.length) {
+			result = results[i].result;
 		}
 	}
 	printf("result: number:%d length:%d\n", result.number, result.length);
+}
+
+void* thread_main(void *arg) {
+	struct WorkEntry* work;
+	work = arg;
+	work->result = main1(work->upper, work->lower);
+
+	return NULL;
 }
 
 struct CacheEntry main1(int start, int end) {
